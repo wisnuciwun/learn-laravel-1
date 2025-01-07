5ce43cb5352baafa741fd08ae0727bfc4084a940
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-01-07 11:06:40 +0700

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:5002', 'https://barayaswarga.com/'],
 
     'allowed_origins_patterns' => [],
 

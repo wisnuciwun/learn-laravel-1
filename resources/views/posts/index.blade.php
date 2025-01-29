a5115313512d930843adc10ac54312b600f8b98c
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-01-29 07:48:23 +0700

    fix: sass 2

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,7 +40,7 @@
                     </select>
                 </div>
             </form>
-            <div class="d-lg-flex d-none flex-wrap gap-4">
+            <div class="d-lg-flex d-none flex-wrap gap-4 justify-content-between">
                 @if (count($data) > 0)
                     @foreach ($data as $item)
                         <a href="{{ route('post.show', $item->id) }}"

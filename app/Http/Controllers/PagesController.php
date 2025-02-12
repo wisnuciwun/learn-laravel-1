2284dc74654a8de64e865b5e8f1d2978d6e7d1fd
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-02-12 08:44:16 +0700

    fix: keypass nullable on edit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -198,7 +198,7 @@ class PagesController extends Controller
             'product_images.*' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'description' => 'nullable|string',
             'tags' => 'nullable|string|max:150',
-            'keypass' => 'required|string|max:150',
+            'keypass' => 'nullable|string|max:150',
         ]);
 
         $imagePaths = [];

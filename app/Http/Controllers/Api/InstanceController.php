5ddc173e12a2d8be82d18a73b11a9bd16d4c4bd6
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-05-31 09:49:18 +0700

    ffix: trial relation 4

--- app/Http/Controllers/Api/InstanceController.php
+++ app/Http/Controllers/Api/InstanceController.php
@@ -30,11 +30,11 @@ class InstanceController extends Controller
                $res = Apps::when($request->keyword, function ($query) use ($request) {
                     return $query->where('name', 'like', "%{$request->keyword}%");
                })
-                    ->with([
-                         'userPriviledge' => function ($q) use ($userId) {
-                              $q->where('user_id', $userId);
-                         }
-                    ])
+                    // ->with([
+                    //      'userPriviledge' => function ($q) use ($userId) {
+                    //           $q->where('user_id', $userId);
+                    //      }
+                    // ])
                     ->get();
 
                // Step 2: Manually load instancePriviledge for each app with filtering

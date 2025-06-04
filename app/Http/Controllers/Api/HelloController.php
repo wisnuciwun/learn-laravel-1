b4ee21cdeb192b7122540da7dd703eb8202ad55b
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-04 08:09:11 +0700

    ffix: trial empty img check 1

--- app/Http/Controllers/Api/HelloController.php
+++ app/Http/Controllers/Api/HelloController.php
@@ -92,7 +92,7 @@ class HelloController extends Controller
                $data = InstanceSettings::where('instance_code', $request->instance_code)->first();
 
                if (!empty($request->img_heading)) {
-                    if (isset($data->img_heading)) {
+                    if (!empty($data->img_heading)) {
                          $image = ItsHelper::saveImage('client', true, $data->img_heading, $request, 'img_heading');
                          $dataToSave['img_heading'] = $image;
                     } else {

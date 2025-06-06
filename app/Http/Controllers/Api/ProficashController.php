140c965a3508e3e504b16820936fcced3ed0f1df
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-06 15:27:51 +0700

    ffeat: proficash api get transactions

--- app/Http/Controllers/Api/ProficashController.php
+++ app/Http/Controllers/Api/ProficashController.php
@@ -21,6 +21,61 @@ use App\Http\Controllers\Controller;
 
 class ProficashController extends Controller
 {
+     public function getTransactions(Request $request)
+     {
+          $userData = ItsHelper::verifyToken($request->token);
+          $request->merge([
+               'instance_id' => $userData->instance->id,
+               'user_id' => $userData->id,
+          ]);
+
+          $success = true;
+          $errors = '';
+          $data = [];
+
+          try {
+               $data = TransactionsIn::with('inventory:name,image,price,base_price')
+                    ->where('instance_id', $request->instance_id)
+                    ->when($request->start_date != '' && $request->end_date != '', function ($q) use ($request) {
+                         $q->whereBetween('created_at', [$request->start_date . " 00:00:00", $request->end_date . ' 23:59:59']);
+                    })
+                    ->when($request->start_date == '' && $request->end_date == '', function ($q) {
+                         $q->whereBetween('created_at', [Carbon::now()->firstOfMonth(), Carbon::now()->endOfMonth()]);
+                    })
+                    ->get();
+
+               $totalSales = 0;
+               $totalBasePrice = 0;
+
+               foreach ($data as $transaction) {
+                    $totalSales += $transaction->price * $transaction->quantity;
+
+                    if ($transaction->inventory) {
+                         $totalBasePrice += $transaction->inventory->base_price * $transaction->quantity;
+                    }
+               }
+
+               return response()->json([
+                    'success' => $success,
+                    'message' => $errors ? '' : "Successfully saved transaction",
+                    'data' => [
+                         'transactions' => $data,
+                         'total_sales' => $totalSales,
+                         'total_base_price' => $totalBasePrice,
+                         'employee_sallary' => 0,
+                         'additional_cost' => 0
+                    ],
+                    'errors' => $errors
+               ], 200);
+          } catch (\Throwable $th) {
+               return response()->json([
+                    'success' => false,
+                    'errors' => $th->getMessage(),
+               ], 500);
+          }
+
+     }
+
      public function addTransactionIn(Request $request)
      {
           $userData = ItsHelper::verifyToken($request->token);
@@ -53,8 +108,7 @@ class ProficashController extends Controller
                $dataInventory = Inventory::where('id', $request->inventory_id)->first();
 
                if ($dataInstance && $dataInventory) {
-
-                    $data = TransactionsIn::create($dataToCreate)->save();
+                    $data = TransactionsIn::insert($dataToCreate);
                } else {
                     $success = false;
                     $errors = 'Required data not found';

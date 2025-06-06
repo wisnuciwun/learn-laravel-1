297dab579eab462c1c78857f83484e11305ef5c2
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-06 17:49:59 +0700

    ffix: id on inventory

--- app/Http/Controllers/Api/ProficashController.php
+++ app/Http/Controllers/Api/ProficashController.php
@@ -34,7 +34,7 @@ class ProficashController extends Controller
           $data = [];
 
           try {
-               $data = TransactionsIn::with('inventory:name,image,price,base_price')
+               $data = TransactionsIn::with('inventory:id,name,image,price,base_price')
                     ->where('instance_id', $request->instance_id)
                     ->when($request->start_date != '' && $request->end_date != '', function ($q) use ($request) {
                          $q->whereBetween('created_at', [$request->start_date . " 00:00:00", $request->end_date . ' 23:59:59']);

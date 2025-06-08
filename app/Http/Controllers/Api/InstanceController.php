bc90f16db58d862b0e9dda9ae65a22beaf004b77
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-09 05:55:57 +0700

    fix: add phone on instance setting

--- app/Http/Controllers/Api/InstanceController.php
+++ app/Http/Controllers/Api/InstanceController.php
@@ -56,7 +56,7 @@ class InstanceController extends Controller
                }
                $dataUser = User::where('id', $userId)->select('name', 'gender', 'referral_code', 'email', 'address', 'income', 'outcome', 'target_per_month', 'saving', 'active', 'is_owner')->first();
                $dataInstance = Instances::where('id', $instanceId)->select('name', 'instance_code')->get();
-               $dataInstanceSettings = InstanceSettings::where('instance_code', $instanceCode)->select('hello_template_id', 'instance_code', 'title', 'slogan', 'promotion', 'third_party_links', 'img_heading')->first();
+               $dataInstanceSettings = InstanceSettings::where('instance_code', $instanceCode)->select('hello_template_id', 'instance_code', 'title', 'slogan', 'promotion', 'third_party_links', 'img_heading', 'phone')->first();
                $dataRole = UserPriviledges::with(['role'])->where('user_id', $userId)->select('id', 'role_id')->first();
 
                return response()->json([

e23b40125be98df51d59709e48b2ac0e98b97816
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-08 22:06:06 +0700

    ffeat: add role on profile

--- app/Http/Controllers/Api/InstanceController.php
+++ app/Http/Controllers/Api/InstanceController.php
@@ -6,6 +6,7 @@ use App\Models\Fianut\Apps;
 use App\Models\Fianut\InstancePriviledges;
 use App\Models\Fianut\Instances;
 use App\Models\Fianut\InstanceSettings;
+use App\Models\Fianut\UserPriviledges;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
@@ -23,13 +24,13 @@ class InstanceController extends Controller
           $instanceCode = $userData->instance_code;
 
           try {
-               $res = User::with(['userPriviledges.app:id,name,link', 'userPriviledges.role:id,name,tabs'])->
+               $data = User::with(['userPriviledges.app:id,name,link', 'userPriviledges.role:id,name,tabs'])->
                     select('id', 'name', 'gender', 'is_owner', 'email', 'address', 'instance_code', 'active', 'nickname')->where('instance_code', $instanceCode)->where('is_owner', '!=', 1)->get();
 
                return response()->json([
                     'success' => true,
                     'message' => 'Get employees successfully',
-                    'data' => $res,
+                    'data' => $data,
                ], 200);
           } catch (\Throwable $th) {
                return response()->json([
@@ -47,17 +48,25 @@ class InstanceController extends Controller
           $instanceCode = $userData->instance->instance_code;
 
           try {
-               $resUser = User::where('id', $userId)->select('name', 'gender', 'referral_code', 'email', 'address', 'income', 'outcome', 'target_per_month', 'saving', 'active', 'is_owner')->first();
-               $resInstance = Instances::where('id', $instanceId)->select('name', 'instance_code')->get();
-               $resInstanceSettings = InstanceSettings::where('instance_code', $instanceCode)->select('hello_template_id', 'instance_code', 'title', 'slogan', 'promotion', 'third_party_links', 'img_heading')->first();
+               if ($userData->is_owner == 1) {
+                    $dataUser = User::where('id', $userId)->select('name', 'gender', 'referral_code', 'email', 'address', 'income', 'outcome', 'target_per_month', 'saving', 'active', 'is_owner')->first();
+               } else {
+                    $dataUser = User::where('id', $userId)->select('name', 'gender', 'referral_code', 'email', 'address', 'active', 'is_owner')->first();
+
+               }
+               $dataUser = User::where('id', $userId)->select('name', 'gender', 'referral_code', 'email', 'address', 'income', 'outcome', 'target_per_month', 'saving', 'active', 'is_owner')->first();
+               $dataInstance = Instances::where('id', $instanceId)->select('name', 'instance_code')->get();
+               $dataInstanceSettings = InstanceSettings::where('instance_code', $instanceCode)->select('hello_template_id', 'instance_code', 'title', 'slogan', 'promotion', 'third_party_links', 'img_heading')->first();
+               $dataRole = UserPriviledges::with(['role'])->where('user_id', $userId)->select('id', 'role_id')->first();
 
                return response()->json([
                     'success' => true,
                     'message' => 'Get profile successfully',
                     'data' => [
-                         'user' => $resUser,
-                         'instance' => $resInstance,
-                         'instance_settings' => $resInstanceSettings,
+                         'user' => $dataUser,
+                         'instance' => $dataInstance,
+                         'instance_settings' => $dataInstanceSettings,
+                         'role' => $dataRole,
                     ],
                ], 200);
           } catch (\Throwable $th) {
@@ -75,7 +84,7 @@ class InstanceController extends Controller
           $userId = $userData->id;
 
           try {
-               $res = Apps::with([
+               $data = Apps::with([
                     'instancePriviledge' => function ($query) use ($instanceId) {
                          $query->where('instance_id', $instanceId);
                     },
@@ -87,7 +96,7 @@ class InstanceController extends Controller
                return response()->json([
                     'success' => true,
                     'message' => 'Get priviledge apps list successfully',
-                    'data' => $res,
+                    'data' => $data,
                ], 200);
           } catch (\Throwable $th) {
                return response()->json([
@@ -106,12 +115,12 @@ class InstanceController extends Controller
                     'user_id' => $userData->id,
                ]);
 
-               $res = Instances::where('id', $request->instance_id)->first();
+               $data = Instances::where('id', $request->instance_id)->first();
 
                return response()->json([
                     'success' => true,
                     'message' => 'Get instance data successfully',
-                    'data' => $res,
+                    'data' => $data,
                ], 200);
           } catch (\Throwable $th) {
                return response()->json([
@@ -130,12 +139,12 @@ class InstanceController extends Controller
                     'user_id' => $userData->id,
                ]);
 
-               $res = InstancePriviledges::with(['instances'])->where('id', $request->instance_id)->first();
+               $data = InstancePriviledges::with(['instances'])->where('id', $request->instance_id)->first();
 
                return response()->json([
                     'success' => true,
                     'message' => 'Get instance priviledge data successfully',
-                    'data' => $res,
+                    'data' => $data,
                ], 200);
           } catch (\Throwable $th) {
                return response()->json([

45569b231ae9539d9be71bf8d7c054c140cdac70
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-06-18 16:02:26 +0700

    ffix: add instance

--- app/Http/Controllers/Api/InstanceController.php
+++ app/Http/Controllers/Api/InstanceController.php
@@ -194,11 +194,6 @@ class InstanceController extends Controller
                     if ($dataInstancePriviledge && $request->is_owner == 1) {
                          $idRoleAppAdmin = Roles::where('name', 'app_admin')->first();
 
-                         $dataToSave = [
-                              'instance_code' => $request->instance_code,
-                              'user_id' => $userData->id,
-                         ];
-
                          // Create instance and get the saved model
                          $dataInstance = Instances::create($dataToSave);
 

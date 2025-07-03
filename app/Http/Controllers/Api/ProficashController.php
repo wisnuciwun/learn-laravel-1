6443b5b238bb5821e33d535223d7214eef0c7eab
wisnuciwun <wisnuadiwardhana@rocketmail.com>
2025-07-03 20:22:15 +0700

    fix: trial fix sallary 2

--- app/Http/Controllers/Api/ProficashController.php
+++ app/Http/Controllers/Api/ProficashController.php
@@ -27,7 +27,7 @@ class ProficashController extends Controller
      {
           $userData = ItsHelper::verifyToken($request->token);
           $request->merge([
-               // 'instance_id' => $userData->instance->id,
+               'instance_code' => $userData->instance_code,
                'user_id' => $userData->id,
           ]);
 
@@ -36,7 +36,7 @@ class ProficashController extends Controller
           $data = [];
 
           try {
-               $dataUser = User::select('name', 'sallary', 'instance_code')->where('instance_code', $request->instance_code)->where('is_owner', 0)->get();
+               $dataUser = User::select('name', 'sallary')->where('instance_code', $request->instance_code)->where('is_owner', 0)->get();
                $dataTransactionIn = TransactionsIn::with('inventory:id,base_price,operational_price')->select('id', 'inventory_id', 'price', 'quantity')
                     ->whereIn('instance_id', $userData->instance->pluck('id')->toArray())
                     ->when($request->start_date && $request->end_date, function ($q) use ($request) {
@@ -82,7 +82,7 @@ class ProficashController extends Controller
                     'total_spending' => $dataTransactionOut->sum(function ($item) {
                          return $item->price * $item->quantity;
                     }),
-                    'employee_sallary' => $dataUser,
+                    'employee_sallary' => $dataUser->sum('sallary'),
                     'total_sold_items' => $dataTransactionIn->sum('quantity'),
                     'total_profit' => $profit,
                     'profit_percentage' => number_format($userData->target_per_month
